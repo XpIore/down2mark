ccf365a4cd404b4b9b24f9f4c2724572d201e4a9
Felix <felix.theissl@students.fhv.at>
2024-11-15 14:36:51 +0100

    Initial commit

---
+++ src/main/java/two/down/bad/guys/down2mark/Down2markApplication.java
@@ -0,0 +1,13 @@
+package two.down.bad.guys.down2mark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Down2markApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Down2markApplication.class, args);
+	}
+
+}
---
+++ src/test/java/two/down/bad/guys/down2mark/Down2markApplicationTests.java
@@ -0,0 +1,13 @@
+package two.down.bad.guys.down2mark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Down2markApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
