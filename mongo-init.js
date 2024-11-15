cb1307a4936fd8cc79f62344f3afc9def52eef5e
Felix <felix.theissl@students.fhv.at>
2024-11-15 17:07:08 +0100

    initial mongo setup files

--- mongo-init.js
+++ mongo-init.js
@@ -6,3 +6,42 @@
 //
 // db = db.getSiblingDB("appdb"); // Switch to the new database
 // db.createCollection("exampleCollection");
+
+db = db.getSiblingDB("MongoDown2Mark")
+
+db.createCollection("Markdown", {
+    validator: {
+        $jsonSchema: {
+            bsonType: "object",
+            required: ["token", "title", "contentVersionPairs"],
+            properties: {
+                token: {
+                    bsonType: "string",
+                    description: "Token must be a string and is required"
+                },
+                title: {
+                    bsonType: "string",
+                    description: "Title must be a string and is required"
+                },
+                contentVersionPairs: {
+                    bsonType: "array",
+                    description: "Must be an array of content-version pairs and is required",
+                    items: {
+                        bsonType: "object",
+                        required: ["content", "version"],
+                        properties: {
+                            content: {
+                                bsonType: "string",
+                                description: "Content must be a string"
+                            },
+                            version: {
+                                bsonType: "string",
+                                description: "Version must be a string representing the content version"
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+});
