61d58fee4278b430ad0bda334cf209d1e5add9b2
Felix <felix.theissl@students.fhv.at>
2024-11-16 09:30:24 +0100

    added dpendencies for redis

--- src/main/java/Services/MarkdownService.java
+++ src/main/java/Services/MarkdownService.java
@@ -2,7 +2,9 @@ package Services;
 
 import Repositories.MarkdownRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class MarkdownService {
     private final MarkdownRepository markdownRepository;
 
