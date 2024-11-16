9aa0844e1e3a47c96b40f6414d740abdeb18a8b2
Felix <felix.theissl@students.fhv.at>
2024-11-16 13:48:21 +0100

    all mapping done?

--- src/main/java/Models/RedisUser.java
+++ src/main/java/Models/RedisUser.java
@@ -1,5 +1,8 @@
 package Models;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 
 import java.io.Serializable;
@@ -7,4 +10,12 @@ import java.io.Serializable;
 @RedisHash("User")
 public class RedisUser implements Serializable {
 
+    @Id
+    @Getter
+    @Setter
+    private String token;
+
+    @Getter
+    @Setter
+    private String content;
 }
